b648e738ff179fa57ac015baf9e0c5a916e7abc0
Raghav <43498142+Raghav10026@users.noreply.github.com>
2020-12-27 22:45:25 +0530

    update: woking

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-var btntranslate = document.querySelector("#btn-translate");
-var txtinput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#txt-output");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-var serverURl = "https://api.funtranslations.com/translate/shakespeare.json";
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/shakespeare.json";
 
 function getTranslationURL(input) {
-  return serverURl + "?" + "text=" + input;
+  return serverURL + "?" + "text=" + input;
 }
 
 function errorHandler(error) {
-  console.log("error occurred", error);
-  alert("Oh Snap 😬! Its not you its us!");
+  console.log("error occured", error);
+  alert("something wrong with server! try again after some time");
 }
 
 function clickHandler() {
-  var inputText = txtinput.value;
+  var inputText = txtInput.value; // taking input
 
+  // calling server for processing
   fetch(getTranslationURL(inputText))
     .then((response) => response.json())
     .then((json) => {
       var translatedText = json.contents.translated;
-      outputDiv.innerText = translatedText;
+      outputDiv.innerText = translatedText; // output
     })
     .catch(errorHandler);
 }
 
-btntranslate.addEventListener("click", clickHandler);
+btnTranslate.addEventListener("click", clickHandler);
